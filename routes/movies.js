120b6a56b21f6c8e696bb7b12ba5bfa6484de6b7
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-12-06 18:48:11 +0300

    test

--- routes/movies.js
+++ routes/movies.js
@@ -8,6 +8,6 @@ router.get('/movies', getMovies);
 router.post('/movies', movieValidateCreate, createMovie);
 router.delete('/movies/:movieId', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
-    movieId: Joi.string().length(24).hex().required(),
+    movieId: Joi.string().required(),
   }),
 }), deleteMovie);
