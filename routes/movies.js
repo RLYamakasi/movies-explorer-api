e488e66465a4cc32891bff17ac478e57897e5361
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-12-14 19:11:59 +0300

    a lot fixes++++++

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,11 @@
 const { celebrate, Joi, Segments } = require('celebrate');
 const router = require('express').Router();
 const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
-const { movieValidateCreate } = require('../validations/movie');
 
 module.exports = router;
 
 router.get('/movies', getMovies);
-router.post('/movies', movieValidateCreate, createMovie);
+router.post('/movies', createMovie);
 router.delete('/movies/:_id', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
     _id: Joi.string().length(24).hex().required(),
