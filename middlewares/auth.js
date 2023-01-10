c8e049f00bed3e6049569739f42760cca845a91a
Alexey Fedorov <mr.lf.1975@mail.ru>
2023-01-10 12:41:39 +0300

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,6 @@ module.exports.auth = (req, res, next) => {
     req.user = tokenCheck;
     return next();
   } catch (err) {
-    console.log(err)
     return next(new ErrorLogin('Ошибка авторизации'));
   }
 };
