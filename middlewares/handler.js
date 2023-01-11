c4e7714d92d7ea7709b10d3e74fce5ef3fe2a7a4
Alexey Fedorov <mr.lf.1975@mail.ru>
2023-01-11 15:56:24 +0300

    test

--- middlewares/handler.js
+++ middlewares/handler.js
@@ -2,6 +2,6 @@ module.exports.errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
-  console.log(err)
+  res.status(statusCode).send({ err });
   next();
 };
