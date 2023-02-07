20ce4e6bfaa9a7a1b2d95ce077aacf4f4bd6e342
Alexey Fedorov <mr.lf.1975@mail.ru>
2023-02-07 21:20:49 +0300

    4комит + lang

--- src/app/App.js
+++ src/app/App.js
@@ -1,32 +1,25 @@
 // import './index.css';
-import Main from '../components/Main';
-import Films from '../components/Films';
-import SavedFilms from '../components/SavedFilms';
-import Registration from '../components/Registration';
-import Login from '../components/Login';
-import Error from '../components/Error';
-import Account from '../components/Account';
+import Main from "../components/Main";
+import Films from "../components/Films";
+import SavedFilms from "../components/SavedFilms";
+import Registration from "../components/Registration";
+import Login from "../components/Login";
+import Error from "../components/Error";
+import Account from "../components/Account";
 import { React, useEffect, useState } from "react";
 import { Routes, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" lang="ru">
       <Routes>
-        <Route path="/" element={<Main/>}>
-        </Route>
-        <Route path="/movies" element={<Films/>}>
-        </Route>
-        <Route path="/saved-movies" element={<SavedFilms/>}>
-        </Route>
-        <Route path="/signup" element={<Registration/>}>
-        </Route>
-        <Route path="/signin" element={<Login/>}>
-        </Route>
-        <Route path="/error" element={<Error/>}>
-        </Route>
-        <Route path="/profile" element={<Account/>}>
-        </Route>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/movies" element={<Films />}></Route>
+        <Route path="/saved-movies" element={<SavedFilms />}></Route>
+        <Route path="/signup" element={<Registration />}></Route>
+        <Route path="/signin" element={<Login />}></Route>
+        <Route path="/error" element={<Error />}></Route>
+        <Route path="/profile" element={<Account />}></Route>
       </Routes>
     </div>
   );
