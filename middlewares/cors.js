d14e571eab289c91eb28c614c26368d39736bf93
Алексей Фёдоров <mr.lf.1975@gmail.com>
2023-07-18 20:48:28 +0300

    cors +

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,23 +11,16 @@ const allowedCors = [
 module.exports.corsCheck = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
+  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, GET, OPTIONS, DELETE, PATCH"
-    );
-  }
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", true);
 
   if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, GET, OPTIONS, DELETE, PATCH"
-    );
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+
     return res.end();
   }
 
