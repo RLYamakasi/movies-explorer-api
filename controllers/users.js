f5550ec5d0a42cf8ad441ded9a8ba152f95d3484
Алексей Фёдоров <mr.lf.1975@gmail.com>
2023-07-19 00:01:35 +0300

    check token

--- controllers/users.js
+++ controllers/users.js
@@ -95,7 +95,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: "none",
         });
-        return res.send({ token });
+        return res.send({ token, res });
       });
     })
     .catch(next);
