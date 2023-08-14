024b0aa2586d4b29793706202c83195452ddd347
lexa <mr.lf.1975@mail.ru>
2023-08-14 08:54:28 +0300

    like add

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
+const { getMovies, createMovie, deleteMovie,setLikeToMovie,deleteLikeFromMovie } = require('../controllers/movies');
 const { movieValidateCreate } = require('../validations/movie');
 const { movieValidateDelete } = require('../validations/movie');
 
@@ -8,3 +8,5 @@ module.exports = router;
 router.get('/movies', movieValidateCreate, getMovies);
 router.post('/movies', createMovie);
 router.delete('/movies/:_id', movieValidateDelete, deleteMovie);
+router.put('/movies/:_id', setLikeToMovie);
+router.delete('/movies/:_id', deleteLikeFromMovie);
